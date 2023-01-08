570e7671ca0be110ee20adc0958c7a80a2a4bf9e
whatwewant <tobewhatwewant@outlook.com>
2023-01-08 18:46:58 +0800

    feat(path): support HomeDir/ConfigDir/JoinCurrentDir/JoinHomeDir/JoinConfigDir

--- path.go
+++ path.go
@@ -1,21 +1,13 @@
 package fs
 
 import (
+	"fmt"
 	"os"
 	ospath "path"
 	"strings"
 
 	"github.com/go-zoox/uuid"
 )
-
-// CurrentDir returns the path of the current directory.
-func CurrentDir() string {
-	if dir, err := os.Getwd(); err == nil {
-		return dir
-	}
-
-	return ""
-}
 
 // JoinPath joins any number of path elements into a single path, adding a
 func JoinPath(paths ...string) string {
@@ -50,4 +42,44 @@ func TmpDirPath() string {
 // TmpFilePath returns the path of the temporary file.
 func TmpFilePath() string {
 	return strings.Join([]string{TmpDirPath(), uuid.V4()}, "/")
+}
+
+// CurrentDir returns the path of the current directory.
+func CurrentDir() string {
+	dir, err := os.Getwd()
+	if err != nil {
+		panic(fmt.Sprintf("cannot get current dir with os.Getwd(): %v", err))
+	}
+
+	return dir
+}
+
+// HomeDir returns the user home directory.
+func HomeDir() string {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		panic(fmt.Sprintf("cannot get user home dir with os.UserHomeDir(): %v", err))
+	}
+
+	return homeDir
+}
+
+// ConfigDir returns the user config directory, which is $HOME/.config
+func ConfigDir() string {
+	return JoinPath(HomeDir(), ".config")
+}
+
+// JoinCurrentDir returns the path which relative with current dir.
+func JoinCurrentDir(relativePath string) string {
+	return JoinPath(CurrentDir(), relativePath)
+}
+
+// JoinHomeDir returns the path which relative with homedir.
+func JoinHomeDir(relativePath string) string {
+	return JoinPath(HomeDir(), relativePath)
+}
+
+// JoinConfigDir returns the path which relative with user home config dir.
+func JoinConfigDir(relativePath string) string {
+	return JoinPath(ConfigDir(), relativePath)
 }
